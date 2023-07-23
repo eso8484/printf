87d958b2c312d079a308673122341b0a7c0a41db
Enejo Sunday Oche <enejosundayoche@gmail.com>
2023-07-23 13:16:33 -0700

    update _printf

--- _printf.c
+++ _printf.c
@@ -1,55 +1,46 @@
 #include "main.h"
-
 /**
-* _printf - To implement the standard library printf function
-* @format: format string
-* @...: The variable arguments
-* Return: number of character
-*/
-
+ * _printf - To implement the standard library printf function
+ * @format: format string
+ * Return: number of character
+ */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	va_start(args, format);
 	int counter = 0;
+	va_list args;
 
-
-	while(*format)
+	va_start(args, format);
+	while (*format)
 	{
 		if (*format == '%')
-		
-			{
-				format++;
-				if(*format == '%')
-				{
-					_putchar('%');
-					counter++;
-				}
-				else if (*format == 'c')
-				{
-					int _c = va_arg(args, int);
-					_putchar(_c);
-					counter++;
-				}
-				else if (*format == 's')
-				{
-					char *str = va_arg(args, char *);
-					while (*str)
-					{
-						_putchar(*str);
-						str++;
-						counter++;
-					}
-				}
-
-			}
-		else
 		{
-			_putchar(*format);
-			counter++;
+			format++;
+			if (*format == '%')
+			{
+				_putchar('%');
+				counter++;
+			}
+			else if (*format == 'c')
+			{
+				int _c = va_arg(args, int);
+
+				_putchar(_c);
+				counter++;
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(args, char *);
+
+				while (*str)
+				{
+					_putchar(*str);
+					str++;
+					counter++;
+				}
+			}
+
 		}
 		format++;
-
 	}
 	va_end(args);
 	return (counter);
