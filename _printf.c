22b6257e43e5c0854fd5d2aa03d00319a05c7154
Brian Shayamano <bshayamano2002@gmail.com>
2023-07-23 12:00:00 -0700

    _putchar.c

--- _printf.c
+++ _printf.c
@@ -3,12 +3,16 @@
 /**
 * _printf - To implement the standard library printf function
 * @format: format string
+* @...: The variable arguments
 * Return: number of character
 */
 
 int _printf(const char *format, ...)
 {
-	int count = 0;
+	va_list args;
+	va_start(args, format);
+	int counter = 0;
+
 
 	while(*format)
 	{
@@ -16,13 +20,37 @@ int _printf(const char *format, ...)
 		
 			{
 				format++;
-				if(*format == '%s')
+				if(*format == '%')
 				{
-					_putchar
+					_putchar('%');
+					counter++;
+				}
+				else if (*format == 'c')
+				{
+					int _c = va_arg(args, int);
+					_putchar(_c);
+					counter++;
+				}
+				else if (*format == 's')
+				{
+					char *str = va_arg(args, char *);
+					while (*str)
+					{
+						_putchar(*str);
+						str++;
+						counter++;
+					}
 				}
 
 			}
+		else
+		{
+			_putchar(*format);
+			counter++;
+		}
+		format++;
+
 	}
-
-
+	va_end(args);
+	return (counter);
 }
