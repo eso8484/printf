8693fe1291f2317f8b03ef4873cd503ee913fd38
Brian Shayamano <bshayamano2002@gmail.com>
2023-07-23 14:47:00 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,5 +1,5 @@
 #include "main.h"
-void print_buffer(char buffer[], int *buff_ind);
+
 /**
  * _printf - Printf function
  * @format: format.
@@ -43,6 +43,7 @@ int _printf(const char *format, ...)
 	va_end(list);
 	return (printed_chars);
 }
+
 /**
  * print_buffer - Prints the contents of the buffer if it exist
  * @buffer: Array of chars
