ced3aede571a7067873f38add2e00325aadebd69
Brian Shayamano <bshayamano2002@gmail.com>
2023-07-24 07:44:36 -0700

    done

--- _printf.c
+++ _printf.c
@@ -1,92 +1,67 @@
 #include "main.h"
 
-int _printf(const char *format, ...);
-int print_char(va_list args);
-int print_string(va_list args);
+void print_buffer(char buffer[], int *buff_ind);
 
 /**
-  * _putchar - Prints character
-  * @c: The character to be printed
-  * Return: Character
-  */
-int _putchar(char c)
-{
-return (write(1, &c, 1));
-}
-/**
-* _printf - Prints the characters and strings
-* @format: The constant of format specifiers
-* Return: The number of thing printed(counter)
-*/
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
+ */
 int _printf(const char *format, ...)
 {
-int counter = 0;
-va_list list;
-va_start(list, format);
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
 
-while (*format)
-{
-if (*format == '%')
-{
-format++;
-if (*format == '%')
-{
-_putchar('%');
-}
-else
-{
-get_op oper[] = {
-{'c', print_char},
-{'s', print_string},
-{'\0', NULL}
-};
+	if (format == NULL)
+		return (-1);
 
-int i = 0;
-while (oper[i].op != '\0')
-{
-if (oper[i].op == *format)
-{
-int result = oper[i].fptr(list);
-if (result < 0)
-return (-1);
-counter++;
-break;
-}
-i++;
-}
-}
-}
-else
-{
-_putchar(*format);
-}
-format++;
+	va_start(list, format);
+
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
+		}
+		else
+		{
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
+		}
+	}
+
+	print_buffer(buffer, &buff_ind);
+
+	va_end(list);
+
+	return (printed_chars);
 }
 
-va_end(list);
-return (counter);
-}
 /**
-* print_char - For printing just a single character
-* @args: The arguments taken
-* Return: A character
-*/
-int print_char(va_list args)
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
 {
-char c = va_arg(args, int);
-return (_putchar(c));
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
 }
-/**
-* print_string - Prints the long strong
-* @args: The range of character it takes
-* Return: The string or -1 when unsuccessful
-*/
-int print_string(va_list args)
-{
-char *str = va_arg(args, char*);
-if (str == NULL)
-{
-return (-1);
-}
-return (write(1, str, strlen(str)));
-}
+
